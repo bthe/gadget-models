66ff88e9e966c8f0bb2ff7ac9a171a19403bbebb
bthe <bjarki.elvarsson@gmail.com>
2015-10-13 20:13:12 +0000

    Saithe iterative reweighting run defined

--- 03-saithe/00-data/run.R
+++ 03-saithe/00-data/run.R
@@ -1,9 +1,7 @@
 library(Rgadget)
 tmp <- gadget.iterative(rew.sI=TRUE,
                         main='main',
-                        grouping=list(gp1=c('si.gp1','si.gp1a'),
-                          gp2=c('si.gp2','si.gp2a'),
-                          gp3=c('si.gp3','si.gp3a')),
+                        grouping=list(gp1=c('si.gp1','si.gp2','si.gp3')),
                         wgts='WGTS')
 
 
