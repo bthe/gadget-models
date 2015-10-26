b005eb3814be58c6ffa3c69ce88678802f12731c
bthe <bjarki.elvarsson@gmail.com>
2015-10-26 14:00:51 +0000

    more buxfixs

--- scenarios.R
+++ scenarios.R
@@ -1,60 +1,67 @@
-library(Rgadget)
-library(doMC)
-registerDoMC(cores = detectCores(logical = TRUE))
-
-runner <- function(bmt=1,lln=1,gil=1,folder='01-cod/02-mfdbcod'){
-  curr.dir <- getwd()
-  setwd(folder)
-  fit <- gadget.fit()
+  library(Rgadget)
+  library(doMC)
+  registerDoMC(cores = detectCores(logical = TRUE))
   
-  ## predict into the future
-  pre.fleet <- filter(fit$fleet.info,year==2014) %>% 
-    select(fleet, ratio = harv.rate) %>%
-    mutate(ratio = ifelse(fleet=='bmt',bmt*ratio,
-                          ifelse(fleet=='lln',lln*ratio,
-                                 gil*ratio)))
+  runner <- function(id=1,bmt=1,lln=1,gil=1,folder='01-cod/02-mfdbcod'){
+    print(folder)
+    curr.dir <- getwd()
+    setwd(folder)
+    print(getwd())#
+    load('WGTS/WGTS.Rdata')
+    #fit <- gadget.fit()
+    fit <- out
+    ## predict into the future
+    pre.fleet <- filter(fit$fleet.info,year==2014) %>% 
+      select(fleet, ratio = harv.rate) %>%
+      mutate(ratio = ifelse(fleet=='bmt',bmt*ratio,
+                            ifelse(fleet=='lln',lln*ratio,
+                                   gil*ratio)))
+    
+    progn <- gadget.forward(params.file='WGTS/params.final',
+                            effort=seq(0.8,1.5,by = 0.05),
+                            fleets=pre.fleet,mat.par=c(0,0),
+                            rec.window = c(1980,2010),
+                            years = 100,
+                            num.trials = 100,
+                            gd = list(dir = ".", 
+                                      rel.dir = sprintf("PRE-%s",id)))
+    
+    progn.ssb <- filter(progn$lw,grepl('mat',stock))
+    res <-
+      left_join(progn$catch %>%
+                  group_by(year,trial,effort) %>%
+                  summarise(catch=sum(biomass.consumed)),
+                progn.ssb) %>%
+      filter(year > 2050) %>%
+      ungroup() %>%
+      group_by(effort) %>%
+      summarise(catch.m=mean(catch),
+                catch.u=quantile(catch,0.975),
+                catch.l=quantile(catch,0.025),
+                ssb.m=mean(total.bio),
+                ssb.u=quantile(total.bio,0.975),
+                ssb.l=quantile(total.bio,0.025)) %>%
+      mutate(bmt=bmt,lln=lln,gil=1)
+    setwd(curr.dir)
+      return(res)  
+  }
   
-  progn <- gadget.forward(params.file='WGTS/params.final',
-                          effort=seq(0.8,1.5,by = 0.05),
-                          fleets=pre.fleet,mat.par=c(0,0),
-                          rec.window = c(1980,2010),
-                          years = 100,
-                          num.trials = 100)
+  dirs <- c('01-cod/02-mfdbcod','02-haddock/02-mfdbhad','03-saithe/02-mfdbpok',
+            '06-ling/02-mfdbling','08-tusk/02-mfdbtusk')
   
-  progn.ssb <- filter(progn$lw,grepl('mat',stock))
-  res <-
-    left_join(progn$catch %>%
-                group_by(year,trial,effort) %>%
-                summarise(catch=sum(biomass.consumed)),
-              progn.ssb) %>%
-    filter(year > 2050) %>%
-    ungroup() %>%
-    group_by(effort) %>%
-    summarise(catch.m=mean(catch),
-              catch.u=quantile(catch,0.975),
-              catch.l=quantile(catch,0.025),
-              ssb.m=mean(total.bio),
-              ssb.u=quantile(total.bio,0.975),
-              ssb.l=quantile(total.bio,0.025)) %>%
-    mutate(bmt=bmt,lln=lln,gil=1)
-  setwd(curr.dir)
-    return(res)  
-}
-
-dirs <- c('01-cod/02-mfdbcod','02-haddock/02-mfdbhad','03-saithe/02-mfdbpok',
-          '06-ling/02-mfdbling','08-tusk/02-mfdbtusk')
-
-lln.inc <- c(1,1.1,1.2,1.3)
-
-run.dat <- data.frame(run.id = 1:length(dirs)*length(lln.inc), 
-                      dirs = rep(dirs,length(lln.inc)),
-                      lln = rep(lln.inc,each=length(dirs)),
-                      bmt = rep(2-lln.inc,each=length(dirs)),
-                      gil = 1)
+  lln.inc <- c(1,1.1,1.2,1.3)
+  
+  run.dat <- data.frame(run.id = 1:(length(dirs)*length(lln.inc)), 
+                        dirs = rep(dirs,length(lln.inc)),
+                        lln = rep(lln.inc,each=length(dirs)),
+                        bmt = rep(2-lln.inc,each=length(dirs)),
+                        gil = 1,
+                        stringsAsFactors = FALSE)
 
 res <- ddply(run.dat,~run.id,
-             function(x) runner(bmt=x$bmt,lln = x$lln,
-                                gil=x$gil, folder=x$dirs),
+             function(x) runner(id=x$run.id[1],
+                                bmt=x$bmt[1], lln = x$lln[1],
+                                gil=x$gil[1], folder=x$dirs[1]),
              .parallel = TRUE)
 
 save(res,file='res.RData')
