7a90174018f3411d20884d5c05f624598bde0e01
Bjarki Þór Elvarsson <bthe@hafro.is>
2016-04-08 12:24:01 +0000

    LIN: SHT->BMT

--- 06-ling/00-data/setup-fleets.R
+++ 06-ling/00-data/setup-fleets.R
@@ -7,7 +7,7 @@ lln.landings <- mfdb_sample_count(mdb, c('age', 'length'), c(list(
 
 
 bmt.landings <- mfdb_sample_count(mdb, c('age', 'length'), c(list(
-    gear=c('BMT','NPT','DSE','PSE','PGT'),
+    gear=c('BMT','NPT','DSE','PSE','PGT','SHT'),
     sampling_type = 'LND',
     species = defaults$species),
                                                         defaults))
